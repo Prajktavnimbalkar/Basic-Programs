0583a61e6211cb04dd62475cc49e5b96ede32ab3
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-07-18 09:35:19 +0530

    Add files via upload

---
+++ SwapApproach2.java
@@ -0,0 +1,25 @@
+package BasicPrograms;
+import java.util.Scanner;
+
+public class SwapApproach2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the value for a : ");
+		int a = scanner.nextInt();
+		System.out.println("Enter the value for b : ");
+		int b = scanner.nextInt();
+		
+		System.out.println("Before swapping a & b : ");
+		System.out.println("a = "+a+", b = "+b);
+		
+		//using *, / operators
+		a = a * b;
+		b = a / b;
+		a = a / b;
+		
+		System.out.println("After swapping a & b : ");
+		System.out.println("a = "+a+", b = "+b);
+		
+		scanner.close();
+	}
+}
